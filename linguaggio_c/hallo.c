c7bf1b56df139e34cc077eac3c69da99b2743e32
userbotspam <79542828+userbotspam@users.noreply.github.com>
2023-09-25 00:30:31 +0200

    o9k

---
+++ linguaggio_c/hallo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hallo world!\n");
+    printf("Hallo world!\n");
+    printf("Hallo world!\n");
+    printf("Hallo world!\n");
+    printf("Hallo world!\n");
+    printf("Hallo world!\n");
+    printf("Hallo world!\n");
+}
+// \n è un carattere speciale che manda a capo
---
+++ python/boh.py
@@ -0,0 +1,5 @@
+import time
+i=0
+while i<= 999:
+    print("@Gionnie Coleman rispondi frocio. numero " + str(i))
+    i+=1
