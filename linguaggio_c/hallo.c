8f02817bb2a8276d814f2193f51c72b4d89c95a3
userbotspam <79542828+userbotspam@users.noreply.github.com>
2023-09-25 23:16:48 +0200

    uni

--- linguaggio_c/hallo.c
+++ linguaggio_c/hallo.c
@@ -3,11 +3,25 @@
 int main()
 {
     printf("Hallo world!\n");
-    printf("Hallo world!\n");
-    printf("Hallo world!\n");
-    printf("Hallo world!\n");
-    printf("Hallo world!\n");
-    printf("Hallo world!\n");
-    printf("Hallo world!\n");
-}
+    // questo è un commento monoriga
 // \n è un carattere speciale che manda a capo
+
+// sviluppato in AT&T, per il sistema operativo Unix.
+    int a,b,r;
+    a = 5;
+    b = 5;
+    r = a+b;   //commento a caso 
+    printf("I l risultato è %d \n", r);
+    /*questo è 
+    un commento 
+    multiriga*/
+    // non si possono inserire commenti nei commenti.
+
+
+    //una variabile di tipo char è grande un byte, e usa i caratteri ASCII
+    // char b;
+    // b = 'b';
+    // printf(b);
+
+     
+}
