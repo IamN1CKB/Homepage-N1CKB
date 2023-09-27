e7d50ccf128e7aa7f5974196cf6581f9e61ecef9
userbotspam <79542828+userbotspam@users.noreply.github.com>
2023-09-27 12:21:45 +0200

    uni

--- python/boh.py
+++
@@ -1,5 +0,0 @@
-import time
-i=0
-while i<= 999:
-    print("@Gionnie Coleman rispondi frocio. numero " + str(i))
-    i+=1
--- python/hyper.py
+++
@@ -1,8 +0,0 @@
-def hyper(z):
-    y=37.300 +z*0.25
-    x=3.20 + y + z*50
-    if x>=50:
-        x=x-50
-        z+=1
-    print(x)
-hyper(0)
