8d2748f7fb327a7976f3d1ad6d8ad4cbf80d98df
userbotspam <79542828+userbotspam@users.noreply.github.com>
2025-03-09 22:33:37 +0100

    giada

---
+++ pagine varie/ti amo a video.js
@@ -0,0 +1,24 @@
+function pausa(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+let titolo = document.getElementById("h1");
+let paragrafo = document.getElementById("p");
+
+
+let frase = "Ti amo Giada";
+let fraseStamp = "";
+async function ciclo() {
+    for (let i = 0; i < frase.length; i++) {
+        let char = 32;
+        while (String.fromCharCode(char) != frase[i]) {
+                char++;
+                titolo.textContent = fraseStamp + String.fromCharCode(char);
+                await pausa(20);
+            }
+            
+            fraseStamp = fraseStamp + String.fromCharCode(char);
+        }
+        
+    paragrafo.textContent = "più di tantissimo <3";
+}
+ciclo();
