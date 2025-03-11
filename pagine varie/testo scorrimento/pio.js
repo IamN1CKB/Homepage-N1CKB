98481e403f1e0313af0a61e071593846fc811ac6
userbotspam <79542828+userbotspam@users.noreply.github.com>
2025-03-11 23:14:43 +0100

    pio

---
+++ pagine varie/testo scorrimento/pio.js
@@ -0,0 +1,25 @@
+function pausa(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+let titolo = document.getElementById("h1");
+let paragrafo = document.getElementById("p");
+
+
+let frase = "Contatta quello stronzo di Carmine Verde";
+let fraseStamp = "";
+async function ciclo() {
+    for (let i = 0; i < frase.length; i++) {
+        let char = 32;
+        while (String.fromCharCode(char) != frase[i]) {
+                char++;
+                titolo.textContent = fraseStamp + String.fromCharCode(char);
+                await pausa(20);
+            }
+            
+            await pausa(20);
+            fraseStamp = fraseStamp + String.fromCharCode(char);
+        }
+        
+    paragrafo.textContent = ""; //"più di tantissimo <3";
+}
+ciclo();
