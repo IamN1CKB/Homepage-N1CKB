1352e3e3b5518824e050e1e67eb2da65752e4f6e
userbotspam <79542828+userbotspam@users.noreply.github.com>
2025-03-17 21:49:12 +0100

    giad

---
+++ giady/Mi ami/do_you_love_me.js
@@ -0,0 +1,28 @@
+const questionContainer = document.querySelector(".question-container");
+const resultContainer = document.querySelector(".result-container.yes");
+const gifResult = document.querySelector(".gif-result");
+const heartLoader = document.querySelector(".cssload-main");
+const yesBtn = document.querySelector(".js-yes-btn");
+const noBtn = document.querySelector(".js-no-btn");
+
+// /change the postion of no button
+noBtn.addEventListener("mouseover", () => {
+  const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
+  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
+
+  noBtn.style.left = `${newX}px`;
+  noBtn.style.top = `${newY}px`;
+});
+
+// yes button functionality
+
+yesBtn.addEventListener("click", () => {
+  questionContainer.style.display = "none";
+  heartLoader.style.display = "inherit";
+
+  const timeoutId = setTimeout(() => {
+    heartLoader.style.display = "none";
+    resultContainer.style.display = "inherit";
+    gifResult.play();
+  }, 3000);
+});
